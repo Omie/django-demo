5cff1aff5982cf24a227c61c479e99c4cdd74d26
Omkarnath Phatak <intelomkar@gmail.com>
2021-12-14 15:27:36 +0530

    added FruitCategory support

--- fruits/models.py
+++ fruits/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+class FruitCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Fruit(models.Model):
     name = models.CharField(max_length=50)
 
     color = models.CharField(max_length=30)
+
+    category = models.ForeignKey(FruitCategory, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
